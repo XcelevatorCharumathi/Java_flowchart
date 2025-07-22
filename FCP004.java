173949dafe06623ef1572c50a4d4a02e12004a7d
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-22 23:11:24 +0530

    FCP004

---
+++ FCP004.java
@@ -0,0 +1,13 @@
+public class FCP004{
+public static void main(String []args)
+{
+int A = 55;
+int B = 56;
+double C = (double) A/B;
+System.out.println(A+B);
+System.out.println(A-B);
+System.out.println(A*B);
+System.out.println(C);
+System.out.println(A%B);
+}
+}
