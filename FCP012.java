e94c44cabc3018d0785e3400a8a4cb8ca514754d
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-23 00:20:10 +0530

    FCP0012

---
+++ FCP012.java
@@ -0,0 +1,15 @@
+public class FCP012{
+public static void main(String []args)
+{
+int Charu = -1243;
+if(Charu>0){
+System.out.println("Positive");
+}
+else if(Charu ==0){
+System.out.println("Zero");
+}
+else{
+System.out.println("Negative");
+}
+}
+}
