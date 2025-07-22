732235ad4793c308c9cf342f9642bc81f14153e1
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-22 13:47:42 +0530

    User input & Print

---
+++ FCP001.java
@@ -0,0 +1,11 @@
+public class FCP001{
+public static void main(String []args)
+{
+int Date = 28;
+int Month = 2;
+int Year = 2010;
+System.out.println(Date);
+System.out.println(Month);
+System.out.println(Year);
+}
+}
