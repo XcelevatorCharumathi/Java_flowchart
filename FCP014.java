970e6c4d28abbc899fbfe415f1ede4fdc117754f
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-23 00:54:09 +0530

    FCP014

---
+++ FCP014.java
@@ -0,0 +1,25 @@
+public class FCP014{
+public static void main(String []args)
+{
+int N1 = 56;
+int N2 = 123;
+int N3 = 546;
+if(N1>N2){
+if(N1>N3){
+System.out.println(N1);
+}
+else{
+System.out.println(N3);
+}
+}
+else{
+ if(N2>N3){
+System.out.println(N2);
+}
+else{
+System.out.println(N3);
+}
+}
+}
+}
+
