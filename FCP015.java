c15bd5b7791c94bfb76cf9550c71ef222eb09451
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-23 00:54:45 +0530

    FCP015

---
+++ FCP015.java
@@ -0,0 +1,31 @@
+public class FCP015{
+public static void main(String []args)
+{
+int A = 5;
+int B = 4;
+int C = 101;
+int D = 10543;
+int Max1;
+int Max2;
+if(A>B){
+Max1 = A;
+}
+else{
+Max1 = B;
+}
+if(C>D){
+Max2 = C;
+}
+else{
+Max2 = D;
+}
+if(Max1>Max2){
+System.out.print(Max1);
+}
+else{
+System.out.print(Max2);
+}
+}
+}
+
+
