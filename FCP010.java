b484817a9335177af99e7224915f8cdd1b5350f9
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-31 22:47:07 +0530

    FCP010.java

--- FCP010.java
+++ FCP010.java
@@ -1,8 +1,9 @@
+import java.util.*;
 public class FCP010{
-public static void main(String []args)
-{
-int A = 32;
-int B = 42;
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int A=Sc.nextInt();
+int B=Sc.nextInt();
 if(A>B){
 System.out.println(A);
 }
