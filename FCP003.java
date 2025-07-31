0abf384409826fb5bd828ddd97f11d828f57d4cb
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-31 22:19:27 +0530

    Update FCP003.java

--- FCP003.java
+++ FCP003.java
@@ -1,8 +1,9 @@
+import java.util.*;
 public class FCP003{
-public static void main(String []args)
-{
-int A = 20;
-int B = 40;
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int A=Sc.nextInt();
+int B=Sc.nextInt(); 
 int C;
 C=A;
 A=B;
