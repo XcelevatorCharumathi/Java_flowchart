29531fcef94dfa4c4f0303ecdc1cc01e79cdef5d
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-31 22:18:31 +0530

    Update FCP002.java

--- FCP002.java
+++ FCP002.java
@@ -1,7 +1,8 @@
+import java.util.*;
 public class FCP002{
-public static void main(String []args)
-{
-String A = " Mumma";
-System.out.println(A);
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+String userWord=Sc.nextLine();
+System.out.println(userWord);
 }
 }
