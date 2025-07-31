74d43a1a8d882d1c0174cbd8177903f8465f635f
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-31 22:22:10 +0530

    FCP006.java

--- FCP006.java
+++ FCP006.java
@@ -1,7 +1,8 @@
+import java.util.*;
 public class  FCP006{
-public static void main(String []args)
-{
-long A = 12300000000000L;
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int A=Sc.nextInt();
 System.out.println(A%10);
 }
 }
