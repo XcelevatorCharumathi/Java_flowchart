cbda2f6270514da6959ea43c85655978091216cd
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-31 22:51:17 +0530

    FCP013.java

--- FCP013.java
+++ FCP013.java
@@ -1,12 +1,13 @@
+import java.util.*;
 public class FCP013{
-public static void main(String []args)
-{
-int N = 10;
-if(N%2==0){
-System.out.println(N);
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int n=Sc.nextInt();
+if(n%2==0){
+System.out.println(n);
 }
 else{
-int R = N*2;
+int R = n*2;
 System.out.println(R);
 }
 }
