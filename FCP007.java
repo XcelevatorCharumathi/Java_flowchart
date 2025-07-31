86959f6d43e6e103d8510caca1e3f417914f45ff
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-31 22:32:22 +0530

    FCP007.java

--- FCP007.java
+++ FCP007.java
@@ -1,9 +1,10 @@
+import java.util.*;
 public class FCP007{
-public static void main(String []args)
-{
-double C= 27;
-double F;
-F = 9*C/5+32;
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int C=Sc.nextInt();
+int F;
+F = 9/5*C+32;
 System.out.println(F);
 }
 }
