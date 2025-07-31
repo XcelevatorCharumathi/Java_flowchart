e7b5b15b4019592567079e3451dcf88a0d0ed197
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-31 22:20:13 +0530

    Update FCP004.java

--- FCP004.java
+++ FCP004.java
@@ -1,13 +1,15 @@
+import java.util.*;
 public class FCP004{
-public static void main(String []args)
-{
-int A = 55;
-int B = 56;
-double C = (double) A/B;
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int A=Sc.nextInt();
+int B=Sc.nextInt(); 
+float C = (float) A/B;
 System.out.println(A+B);
 System.out.println(A-B);
 System.out.println(A*B);
 System.out.println(C);
+System.out.println(A/B);
 System.out.println(A%B);
 }
 }
