7bf225d420d8d96b7f83f1d81d22c542ca3e9cdc
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-31 22:48:20 +0530

    FCP011.java

--- FCP011.java
+++ FCP011.java
@@ -1,7 +1,8 @@
+import java.util.*;
 public class FCP011{
-public static void main(String []args)
-{
-long A = 24L;
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int A=Sc.nextInt();
 if(A%2 ==0){
 System.out.print("Even");
 }
