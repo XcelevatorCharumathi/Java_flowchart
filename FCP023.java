ad5cf0d48d607f4f5f7076f425ccf696652ed371
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-07-31 22:57:30 +0530

    FCP023

---
+++ FCP023.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FCP023{
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int N=Sc.nextInt();
+Sc.nextLine();
+String name=Sc.nextLine();
+for(int i=0; i<N; i++){
+System.out.println(name);
+}
+}
+}
