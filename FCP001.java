2d34ca9ab41e069aa665fdc8e7c7dc28491fa2cd
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-08-02 06:50:31 +0530

    FCP001

--- FCP001.java
+++ FCP001.java
@@ -1,9 +1,11 @@
+import java.util.*;
 public class FCP001{
-public static void main(String []args)
-{
-int Date = 28;
-int Month = 2;
-int Year = 2010;
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int Date=Sc.nextInt();
+Sc.nextLine();
+String Month=Sc.nextLine();
+int Year=Sc.nextInt();
 System.out.println(Date);
 System.out.println(Month);
 System.out.println(Year);
