e81f702753a9159cad2e096949f6e8e467476fb5
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-08-02 06:37:17 +0530

    FCP024

---
+++ FCP024.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FCP024{
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int N=Sc.nextInt();
+for(int i=1; i<N; i++){
+if(i%2==0){
+System.out.println(i);
+}
+}
+}
+}
