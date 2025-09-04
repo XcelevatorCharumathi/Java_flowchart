ab6cd094fffe84c2687fa495d477389ee3d62920
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-09-04 12:37:03 +0530

    FCP073.java

---
+++ FCP073.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class FCP073{
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int M=Sc.nextInt();
+int [ ][ ]arr1=new int[M][M];
+int [ ][ ]arr2=new int[M][M];
+int i=0;
+int j=0;
+for(i=0; i<M; i++){
+for(j=0; j<M; j++){
+arr1[i][j]=Sc.nextInt();
+}
+}
+for(i=0; i<M; i++){
+for(j=0; j<M; j++){
+arr2[i][j]=Sc.nextInt();
+}
+}
+for(i=0; i<M; i++){
+for(j=0; j<M; j++){
+int k=arr1[i][j]+arr2[i][j];
+System.out.println(k);
+}
+}
+}
+}
