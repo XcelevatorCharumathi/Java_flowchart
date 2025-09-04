fa199b611a884c049cd590d55141b94469fbfa99
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-09-04 12:44:40 +0530

    FCP086.java

---
+++ FCP086.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class FCP086{
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int N=Sc.nextInt();
+for(int j=1; j<=N; j++){
+	for(int s=1; s<=N-j; s++){
+	System.out.print("   ");
+	}
+	for(int i= 1; i<=N+j; i++){
+	if(i+j>N+1){
+	System.out.print("* ");
+	}
+}
+System.out.println();
+System.out.println();
+}
+for(int j=N-1; j>0; j--){
+	for(int s=1; s<=N-j; s++){
+	System.out.print("   ");
+	}
+	for(int i= 1; i<=N+j; i++){
+	if(i+j>N+1){
+	System.out.print("* ");
+	}
+}
+System.out.println();
+System.out.println();
+}
+}
+}
